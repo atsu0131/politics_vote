9eb7bbee942ab7e8fee658ad43fbce41062b246e
a <isai@ihitoshitokushinoMacBook-Air.local>
2018-10-29 11:11:45 +0900

    create user controller new create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,46 @@
 class UsersController < ApplicationController
   def index
+    @user = User.all
   end
 
   def new
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_path, notice:"ユーザー#{@user.name}を登録しました"
+    else
+      rendar :new
+    end
+  end
+
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.save
+      redirect_to user_path(@user), notice:"ユーザー#{@user.name}を更新しました"
+    else
+      rendar :new
+    end
   end
 
   def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to user_path(@user),notice:"ユーザー#{@user.name}を削除しました"
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
+  def user_params
+    params.require(:user).permit(:name, :email, :admin, :password, :password_confirmation,:sex,:age)
+  end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :name,  presence: true, length: { maximum: 30 }
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+
+  has_many :active_votes, foreign_key: 'follower_id', class_name: 'Vote', dependent: :destroy
+  has_many :passive_votes, foreign_key: 'followed_id', class_name: 'Vote', dependent: :destroy
+  has_many :following, through: :active_votes, source: :followed
+  has_many :followers, through: :passive_votes, source: :follower
 end
